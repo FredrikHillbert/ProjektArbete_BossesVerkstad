069500790fe2e7acb5650b8d87799bbd50411d7c
Nurleng <70631858+Nono-14@users.noreply.github.com>
2020-10-23 15:52:44 +0200

    update

---
+++ Projektuppgift/Logic/Services/IAdmin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public interface IAdmin
+    {
+        void AddUser();
+
+        void RemoveUser();
+
+        void GetUser();
+
+        void RemoveArenden();
+
+        void GetArenden();
+
+
+    }
+}
